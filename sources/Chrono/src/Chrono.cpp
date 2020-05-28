c84fabe541f8962a2a936529145eb8c0b28a919c
root <root@DESKTOP-IQ305Q4.localdomain>
2020-05-28 21:28:50 +0200

    [ADD] Adding functionnal Cmake for Windows and Linux [TODO] Test it on mac

---
+++ sources/Chrono/src/Chrono.cpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2020
+** IndieStudio
+** File description:
+** Chrono
+*/
+
+#include <ctime>
+#include "Chrono.hpp"
+
+Chrono::Chrono()
+{
+}
+
+Chrono::~Chrono()
+{
+}
+
+std::string Chrono::getTime()
+{
+    auto end = std::chrono::system_clock::now();
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+ 
+    return std::ctime(&end_time);
+}
+
+void Chrono::resetChrono()
+{
+    this->startChrono();
+}
+
+void Chrono::startChrono()
+{
+    this->_begin = std::chrono::system_clock::now();
+}
+
+double Chrono::getElapsedTime()
+{
+    this->_end = std::chrono::system_clock::now();
+    std::chrono::duration<double> result = this->_end - this->_begin;
+    return result.count();
+}
